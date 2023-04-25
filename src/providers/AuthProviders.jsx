4157c233c724062281f8cf9568bec98c39c63a22
md-abir-alahe <mdabiralahe@gmail.com>
2023-04-25 13:09:26 +0600

    add create user and sign in user using context api

--- src/providers/AuthProviders.jsx
+++ src/providers/AuthProviders.jsx
@@ -1,5 +1,5 @@
 import React, {  createContext, useState } from 'react';
-import { getAuth } from "firebase/auth";
+import { createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword } from "firebase/auth";
 import app from '../firebase/firebase.config';
 
 export const AuthContext = createContext(null);
@@ -10,8 +10,19 @@ const AuthProviders = ({children}) => {
 
     const [user, setUser] = useState(null);
 
+    const createUser = (email, password) => {
+        return createUserWithEmailAndPassword(auth, email, password);
+    }
+
+    const signIn = (email, password) => {
+        return signInWithEmailAndPassword(auth, email, password);
+    }
+
+    // sending to all files
     const authInfo = {
         user,
+        createUser,
+        signIn
     }
     return (
         <AuthContext.Provider value={authInfo}>
